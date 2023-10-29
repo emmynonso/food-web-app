731b6517f5b9f757c5edb41b25a9fe770c66f10b
Emmanuel Chinonso <emmanuelchinonso1340@gmail.com>
2023-10-29 07:52:13 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        height: {
+          '128': '32rem',
+        }
+      },
+      
+    },
+    plugins: [],
+  }
+  
